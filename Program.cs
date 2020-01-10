587742c355636939b5233a60d8c9f851e17b2d0f
Gruszczynski.Witold WGI <WGI@kmd.dk>
2020-01-10 13:47:34 +0100

    Init

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DotNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var generator = new FibonaciGenerator();
+            foreach (var digit in generator.Generate(20))
+            {
+                Console.WriteLine(digit);
+            }
+        }
+    }
+}
